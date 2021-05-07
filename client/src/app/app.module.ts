f9dcb7060b96afd9adaab453714648a3d8c4f24e
Taher Irfan <taherhphs@gmail.com>
2021-05-08 03:25:19 +0600

    Interceptor Added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './core/nav-bar/nav-bar.component';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClientModule, HTTP_INTERCEPTORS} from '@angular/common/http';
 import { CoreModule } from './core/core.module';
 import { ShopModule } from './shop/shop.module';
 import { HomeModule } from './home/home.module';
 import { AppRoutingModule } from './app-routing.module';
+import { ErrorInterceptor } from './core/interceptor/error.interceptor';
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +22,9 @@ import { AppRoutingModule } from './app-routing.module';
     CoreModule,
     HomeModule
   ],
-  providers: [],
+  providers: [
+    {provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
